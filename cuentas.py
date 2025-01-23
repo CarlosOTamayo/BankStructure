c79d19c164b1fe07e53a7eb120e00eefb0af9daf
Carlos T <carlosotamayo@gmail.com>
2025-01-23 12:06:41 -0500

    Modi cuentas de usuario y funcionarioç

---
+++ cuentas.py
@@ -0,0 +1,16 @@
+import os
+#Creacion las cuentas
+def Usuario():
+    UsPriNom(input("Ingrese su primer nombre: "))
+    UsSegNom(input("Ingrese su segundo nombre: "))
+    UsPriApe(input("Ingrese su primer apellido: "))
+    UsSegApe(input("Ingrese su segundo apellido: "))
+    UsCedu(int(input("Ingrese su numero de cedula sin espacios: ")))
+
+def Funcionario():
+    FunPriNom(input("Ingrese su primer nombre: "))
+    FunSegNom(input("Ingrese su segundo nombre: "))
+    FunPriApe(input("Ingrese su primer apellido: "))
+    FunSegApe(input("Ingrese su segundo apellido: "))
+    FunCedu(int(input("Ingrese su numero de cedula sin espacios: ")))
+    
